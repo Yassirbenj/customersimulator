67ec03bab640e9eda9487324bbdc893c5a63b4d8
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-09-23 19:17:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,40 @@
 import streamlit as st
+import openai
 
-st.title('🎈 App Name')
+# Set your OpenAI API key here
+api_key = st.secrets["openai"]
 
-st.write('Hello world!')
+# Initialize the OpenAI API client
+openai.api_key = api_key
+
+# Function to simulate the conversation
+def simulate_customer_interaction():
+    st.sidebar.markdown("### Customer Persona")
+    customer_persona = st.sidebar.text_area("Enter the customer persona:")
+    initial_message = st.text_input("You:", "Hello, I have some questions about your product")
+
+    conversation = [customer_persona, initial_message]
+
+    if st.button("Send"):
+        user_input = st.text_input("You:")
+        conversation.append(f"You: {user_input}")
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation,
+        )
+
+        bot_reply = response['choices'][0]['message']['content']
+        st.text("Chatbot:")
+        st.success(bot_reply)
+        conversation.append(f"Chatbot: {bot_reply}")
+
+def main():
+    st.title("Customer Support Chatbot")
+    st.sidebar.title("Settings")
+    st.sidebar.info("This app simulates a customer interacting with a customer support chatbot.")
+
+    simulate_customer_interaction()
+
+if __name__ == "__main__":
+    main()
